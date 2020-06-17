251eaabc8ce88eab03ee595ec023f8fb6b7b53b1
shamita1512 <chemica1cooki3@gmail.com>
2020-06-17 16:56:01 +0530

    Added ground and rectangles

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+  rect1 = new Rectangle(100,50,300,60);
+  rect2 = new Rectangle(400,70,300,60);
+  ground = new Ground(400,400,800,10);
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black"); 
+  Engine.update(engine); 
+  rect1.display();
+  rect2.display();
+  ground.display();
   drawSprites();
 }
